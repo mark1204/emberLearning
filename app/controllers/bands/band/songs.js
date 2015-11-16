fb1c65d460d50ad0ef8cdfd1071eeb5163132660
dreamingmonkey <zyl1204@gmail.com>
2015-11-16 17:51:39 +1100

    Chapter 12 - sorting & searching based on test drive dev.

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -4,6 +4,39 @@ export default Ember.Controller.extend({
 
   title: '',
 
+  searchTerm: '',
+
+  matchingSongs: Ember.computed('model.songs.@each.title', 'searchTerm', function(){
+
+    var searchTerm = this.get('searchTerm').toLowerCase();
+    return this.get('model.songs').filter(function(song){
+      return song.get('title').toLowerCase().indexOf(searchTerm) !== -1;
+    });
+
+  }),
+
+  queryParams:{
+    sortBy: 'sort',
+    searchTerm: 's'
+  },
+
+  sortBy: 'ratingDesc',
+
+  sortProperties: Ember.computed('sortBy', function(){
+
+    var options = {
+                    'ratingDesc': 'rating:desc,title:asc',
+                    'ratingAsc':  'rating:asc,title:asc',
+                    'titleDesc': 'title:desc',
+                    'titleAsc': 'title:asc'
+                  };
+
+    return options[this.get('sortBy')].split(',');
+
+  }),
+
+  sortedSongs: Ember.computed.sort('matchingSongs', 'sortProperties'),
+
   songCreationStarted: false,
 
   canCreateSong: Ember.computed('songCreationStarted', 'model.songs.length', function(){
@@ -26,6 +59,10 @@ export default Ember.Controller.extend({
 
     enableSongCreation: function(){
       this.set('songCreationStarted', true);
+    },
+
+    setSorting: function(option){
+      this.set('sortBy', option);
     }
   }
 });
